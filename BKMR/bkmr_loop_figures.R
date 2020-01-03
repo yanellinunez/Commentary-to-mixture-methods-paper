481b7e8a49c468db61934fb27b5bd76af3fa7c89
lizzyagibson <eag2186@cumc.columbia.edu>
2020-01-03 17:20:34 -0500

    Update lasso CV plot and BKMR exposure-response plot.

--- BKMR/bkmr_loop_figures.R
+++ BKMR/bkmr_loop_figures.R
@@ -18,8 +18,8 @@ library(tidyverse)
 # }
 
 #save(knots_out, file = "./HPC/knots_output.RDA")
-load("./HPC/knots_output.RDA")
-knots_out <- knots_out %>% unnest_wider(plot_dat)
+load("./BKMR/HPC/knots_output.RDA")
+#knots_out <- knots_out %>% unnest_wider(plot_dat)
 
 identical(knots_out$knots[[1]], knots_out$knots[[3]])
 identical(knots_out$pips[[1]], knots_out$pips[[3]])
@@ -31,9 +31,33 @@ knots_out
 ## PIPs
 ##########################################################################################################################
 
-knots_out %>% select(seed, pips) %>% unnest(cols = c(pips)) %>% 
+knots_out %>% 
+  dplyr::select(seed, pips) %>% unnest(cols = c(pips)) %>% 
+  mutate(variable = fct_recode(variable, "PCB 74" = "PCB74",
+                               "PCB 99" = "PCB99",
+                               "PCB 118" = "PCB118",
+                               "PCB 138" = "PCB138",
+                               "PCB 153" = "PCB153",
+                               "PCB 170" = "PCB170",
+                               "PCB 180" = "PCB180",
+                               "PCB 187" = "PCB187",
+                               "PCB 194" = "PCB194",
+                               "1,2,3,6,7,8-hxcdd" = "Dioxin1",
+                               "1,2,3,4,6,7,8-hpcdd" = "Dioxin2",
+                               "1,2,3,4,6,7,8,9-ocdd" =  "Dioxin3",
+                               "2,3,4,7,8-pncdf" =  "Furan1",
+                               "1,2,3,4,7,8-hxcdf" =  "Furan2",
+                               "1,2,3,6,7,8-hxcdf" =  "Furan3",
+                               "1,2,3,4,6,7,8-hxcdf" =  "Furan4",
+                               "PCB 169" =  "PCB169",
+                               "PCB 126" = "PCB126")) %>%
   group_by(variable) %>% 
-  summarize(group_mean = mean(groupPIP), group_sd = sd(groupPIP), ind_mean = mean(condPIP), ind_sd = sd(condPIP))
+  summarise(group_min = min(groupPIP), group_med = median(groupPIP), 
+            group_max = max(groupPIP)
+            , ind_min = min(condPIP),    ind_med = median(condPIP),    ind_max = max(condPIP)) %>% 
+  mutate_if(is.double, round, 4) %>%
+  mutate(diff = (ind_max - ind_med) / ind_med,
+         range = (ind_max - ind_min))
 
 ##########################################################################################################################
 ## Plots
@@ -42,8 +66,52 @@ knots_out %>% select(seed, pips) %>% unnest(cols = c(pips)) %>%
 ##########################################################################################################################
 ## Univariate Exposure-Response Functions
 ##########################################################################################################################
+# Median
+mediank <- knots_out %>% dplyr::select(seed, pred.resp.univar) %>% 
+  unnest(cols = c(pred.resp.univar)) %>%
+  mutate(seed = as.character(seed)) %>%
+  group_by(z, variable) %>% 
+  summarise(mediann = median(est),
+            range = max(est) - min(est),
+            sderr = sd(est))
 
-knots_plot <- knots_out %>% select(seed, pred.resp.univar) %>% 
+# All seeds
+knots_out %>% dplyr::select(seed, pred.resp.univar) %>% 
+  unnest(cols = c(pred.resp.univar)) %>%
+  left_join(., mediank, by = c("z", "variable")) %>% 
+  mutate(variable = fct_recode(variable, "PCB 74" = "PCB74",
+                               "PCB 99" = "PCB99",
+                               "PCB 118" = "PCB118",
+                               "PCB 138" = "PCB138",
+                               "PCB 153" = "PCB153",
+                               "PCB 170" = "PCB170",
+                               "PCB 180" = "PCB180",
+                               "PCB 187" = "PCB187",
+                               "PCB 194" = "PCB194",
+                               "1,2,3,6,7,8-hxcdd" = "Dioxin1",
+                               "1,2,3,4,6,7,8-hpcdd" = "Dioxin2",
+                               "1,2,3,4,6,7,8,9-ocdd" =  "Dioxin3",
+                               "2,3,4,7,8-pncdf" =  "Furan1",
+                               "1,2,3,4,7,8-hxcdf" =  "Furan2",
+                               "1,2,3,6,7,8-hxcdf" =  "Furan3",
+                               "1,2,3,4,6,7,8-hxcdf" =  "Furan4",
+                               "PCB 169" =  "PCB169",
+                               "PCB 126" = "PCB126")) %>%
+  mutate(seed = as.character(seed)) %>%
+  #filter(variable == "2,3,4,7,8-pncdf" | variable == "PCB 74") %>% 
+  ggplot(aes(z, est, group = seed, ymin = est - 1.96*se, ymax = est + 1.96*se)) +
+  geom_hline(yintercept = 00, linetype = "dashed") +
+  geom_smooth(aes(group = seed), size = 0.25, stat = "identity", alpha = 0.1) + 
+  #geom_smooth(aes(x = z, y = mediann, 
+  #                ymin = mediann - 3*sderr, ymax = mediann + 3*sderr), size = 0.5, # med +- sd error, wrong...
+  #            fill = "lightskyblue", alpha = 0.1, stat = "identity") + 
+  facet_wrap(~ variable) +
+  labs(y = "Estimate", x = "Exposure") +
+  theme_minimal() +
+  theme(legend.position = "none", strip.background = element_rect(fill = "white"))
+
+knots_out %>% 
+  dplyr::select(seed, pred.resp.univar) %>% 
   unnest(cols = c(pred.resp.univar)) %>%
   mutate(variable = fct_recode(variable, "PCB 74" = "PCB74",
                                "PCB 99" = "PCB99",
@@ -64,7 +132,8 @@ knots_plot <- knots_out %>% select(seed, pred.resp.univar) %>%
                                "PCB 169" =  "PCB169",
                                "PCB 126" = "PCB126")) %>%
   mutate(seed = as.character(seed)) %>% 
-  ggplot(aes(z, est, group = seed)) +
+  #filter(variable == "1,2,3,4,6,7,8-hxcdf") %>% 
+  ggplot(aes(z, est, ymin = est - se/2, ymax = est + se/2, group = seed)) +
   geom_hline(yintercept = 00, linetype = "dashed", color = "red") +
   geom_smooth(aes(color = seed), stat = "identity") + 
   labs(title = "Univariate Exposure-Response Functions over 100 Knot Seeds",
@@ -72,10 +141,6 @@ knots_plot <- knots_out %>% select(seed, pred.resp.univar) %>%
   facet_wrap(~ variable) + theme_bw() +
   theme(legend.position = "none") +
   theme(strip.background = element_rect(fill = "white"))
-
-# pdf("./Figures/BKMR_knots_plot.pdf")
-# knots_plot
-# dev.off()
 
 # ##########################################################################################################################
 # #### Overall Mixture Effect
@@ -114,7 +179,7 @@ knots_out %>% select(seed, risks.overall) %>%
 
 
 #save(model_out, file = "./HPC/model_output.RDA")
-load("./HPC/model_output.RDA")
+load("./BKMR/HPC/model_output.RDA")
 model_out <- model_out %>% unnest_wider(plot_dat)
 model_out
 
@@ -123,20 +188,12 @@ model_out
 ##########################################################################################################################
 
 model_out %>% 
-  select(seed, pips) %>% unnest(cols = c(pips)) %>% 
+  dplyr::select(seed, pips) %>% unnest(cols = c(pips)) %>% 
   group_by(variable) %>% 
-  summarize(group_mean = mean(groupPIP), group_sd = sd(groupPIP), ind_mean = mean(condPIP), ind_sd = sd(condPIP))
+  summarise(group_mean = mean(groupPIP), group_sd = sd(groupPIP), ind_mean = mean(condPIP), ind_sd = sd(condPIP))
 
-##########################################################################################################################
-## Plots
-##########################################################################################################################
-
-##########################################################################################################################
-## Univariate Exposure-Response Functions
-##########################################################################################################################
-
-model_plot <- model_out %>% select(seed, pred.resp.univar) %>% 
-  unnest(cols = c(pred.resp.univar)) %>%
+model_out %>% 
+  dplyr::select(seed, pips) %>% unnest(cols = c(pips)) %>% 
   mutate(variable = fct_recode(variable, "PCB 74" = "PCB74",
                                "PCB 99" = "PCB99",
                                "PCB 118" = "PCB118",
@@ -155,42 +212,155 @@ model_plot <- model_out %>% select(seed, pred.resp.univar) %>%
                                "1,2,3,4,6,7,8-hxcdf" =  "Furan4",
                                "PCB 169" =  "PCB169",
                                "PCB 126" = "PCB126")) %>%
-  mutate(seed = as.character(seed)) %>% 
-  ggplot(aes(z, est, group = seed)) +
-  geom_hline(yintercept = 00, linetype = "dashed", color = "red") +
-  geom_smooth(aes(color = seed), stat = "identity") + 
-  labs(title = "Univariate Exposure-Response Functions over 100 Model Seeds",
-       y = "Estimate", x = "Exposure") +
-  facet_wrap(~ variable) + theme_bw() +
-  theme(legend.position = "none") +
-  theme(strip.background = element_rect(fill = "white"))
+  group_by(variable) %>% 
+  summarise(group_min = min(groupPIP), group_med = median(groupPIP), group_max = max(groupPIP), 
+            ind_min = min(condPIP),    ind_med = median(condPIP),    ind_max = max(condPIP)) %>% 
+  mutate(diff = (ind_max - ind_med) / ind_med,
+         range = (ind_max - ind_min))
 
-model_out %>% select(seed, pred.resp.univar) %>% 
+##########################################################################################################################
+## Plots
+##########################################################################################################################
+
+##########################################################################################################################
+## Univariate Exposure-Response Functions
+##########################################################################################################################
+
+# Median
+mediann <- model_out %>% dplyr::select(seed, pred.resp.univar) %>% 
   unnest(cols = c(pred.resp.univar)) %>%
+  mutate(seed = as.character(seed)) %>%
+  group_by(z, variable) %>% 
+  summarise(mediann = median(est),
+            range = max(est) - min(est),
+            sderr = sd(est),
+            q25 = quantile(est, probs = 0.25),
+            q75 = quantile(est, probs = 0.75))
+
+# All seeds
+model_plot <- model_out %>% dplyr::select(seed, pred.resp.univar) %>% 
+  unnest(cols = c(pred.resp.univar)) %>%
+  left_join(., mediann, by = c("z", "variable")) %>% 
+  mutate(variable = fct_recode(variable, "PCB 74" = "PCB74",
+                               "PCB 99" = "PCB99",
+                               "PCB 118" = "PCB118",
+                               "PCB 138" = "PCB138",
+                               "PCB 153" = "PCB153",
+                               "PCB 170" = "PCB170",
+                               "PCB 180" = "PCB180",
+                               "PCB 187" = "PCB187",
+                               "PCB 194" = "PCB194",
+                               "1,2,3,6,7,8-hxcdd" = "Dioxin1",
+                               "1,2,3,4,6,7,8-hpcdd" = "Dioxin2",
+                               "1,2,3,4,6,7,8,9-ocdd" =  "Dioxin3",
+                               "2,3,4,7,8-pncdf" =  "Furan1",
+                               "1,2,3,4,7,8-hxcdf" =  "Furan2",
+                               "1,2,3,6,7,8-hxcdf" =  "Furan3",
+                               "1,2,3,4,6,7,8-hxcdf" =  "Furan4",
+                               "PCB 169" =  "PCB169",
+                               "PCB 126" = "PCB126")) %>%
+  mutate(seed = as.character(seed)) %>%
+  #filter(variable == "2,3,4,7,8-pncdf" | variable == "PCB 74") %>% 
+  ggplot(aes(z, est, group = seed)) +
+  geom_hline(yintercept = 00, linetype = "dashed") +
+  geom_smooth(aes(group = seed,
+                  #ymin = est - 1.96*se,
+                  #ymax = est + 1.96*se
+                  ), alpha = 0.01, size = 0.25,
+              color = "blue", stat = "identity") + 
+  #geom_smooth(aes(x = z, y = mediann, 
+  #                ymin = q25, ymax = q75), size = 0.5, # med +- iqr
+  #                fill = "lightskyblue", alpha = 0.1, stat = "identity") + 
+  facet_wrap(~ variable) +
+  labs(y = "Estimate", x = "Exposure") +
+  theme_minimal() +
+  theme(legend.position = "none", strip.background = element_rect(fill = "white"))
+
+pdf("./Figures/BKMR_model_plot.pdf")
+model_plot
+dev.off()
+
+# For paper
+pdf("./Figures/BKMR_2pop_plot.pdf")
+model_out %>% dplyr::select(seed, pred.resp.univar) %>% 
+  unnest(cols = c(pred.resp.univar)) %>%
+  left_join(., mediann, by = c("z", "variable")) %>% 
+  mutate(variable = fct_recode(variable, "PCB 74" = "PCB74",
+                               "PCB 99" = "PCB99",
+                               "PCB 118" = "PCB118",
+                               "PCB 138" = "PCB138",
+                               "PCB 153" = "PCB153",
+                               "PCB 170" = "PCB170",
+                               "PCB 180" = "PCB180",
+                               "PCB 187" = "PCB187",
+                               "PCB 194" = "PCB194",
+                               "1,2,3,6,7,8-hxcdd" = "Dioxin1",
+                               "1,2,3,4,6,7,8-hpcdd" = "Dioxin2",
+                               "1,2,3,4,6,7,8,9-ocdd" =  "Dioxin3",
+                               "2,3,4,7,8-pncdf" =  "Furan1",
+                               "1,2,3,4,7,8-hxcdf" =  "Furan2",
+                               "1,2,3,6,7,8-hxcdf" =  "Furan3",
+                               "1,2,3,4,6,7,8-hxcdf" =  "Furan4",
+                               "PCB 169" =  "PCB169",
+                               "PCB 126" = "PCB126")) %>%
+  mutate(seed = as.character(seed)) %>%
+  filter(variable == "2,3,4,7,8-pncdf" | variable == "PCB 74") %>% 
+  ggplot(aes(z, est, group = seed)) +
+  geom_hline(yintercept = 00, linetype = "dashed") +
+  geom_smooth(aes(group = seed,
+                  #ymin = est - 1.96*se,
+                  #ymax = est + 1.96*se
+  ), alpha = 0.01, size = 0.25,
+  color = "blue", stat = "identity") + 
+  #geom_smooth(aes(x = z, y = mediann, 
+  #                ymin = q25, ymax = q75), size = 0.5, # med +- iqr
+  #                fill = "lightskyblue", alpha = 0.1, stat = "identity") + 
+  facet_wrap(~ variable) +
+  labs(y = "Estimate", x = "Exposure") +
+  theme_minimal(base_size = 15) +
+  theme(legend.position = "none", strip.background = element_rect(fill = "white"))
+dev.off()
+
+model_out %>% dplyr::select(seed, pred.resp.univar) %>% 
+  unnest(cols = c(pred.resp.univar)) %>%
+  left_join(., mediann, by = c("z", "variable")) %>% 
+  mutate(seed = as.character(seed)) %>%
+  filter(variable %in% c("PCB99", "PCB118", "PCB126", "PCB180", "PCB169", "Furan1")) %>% 
   group_by(seed, variable) %>% 
-  filter(any(variable == "Furan1" & est < -0.02)) %>% 
-  summarise(.)
-
-# pdf("./Figures/BKMR_model_plot.pdf")
-# model_plot
-# dev.off()
-
+  #filter(any(z < -1) & any(est < -0.05)) %>% 
+  ggplot(aes(z, est, group = seed,
+             ymin = est - 1.96*se,
+             ymax = est + 1.96*se
+             )) +
+  geom_hline(yintercept = 00, linetype = "dashed") +
+  geom_smooth(aes(group = seed), alpha = 0.01, size = 0.25, stat = "identity") + 
+  #geom_smooth(aes(x = z, y = mediann, 
+  #                ymin = q25, ymax = q75), size = 0.5, # med +- iqr
+  #                fill = "lightskyblue", alpha = 0.1, stat = "identity") + 
+  labs(y = "Estimate", x = "Exposure") +
+  facet_wrap(~variable)
+  theme_minimal()
+  
 # ##########################################################################################################################
 # #### Overall Mixture Effect
 # ##########################################################################################################################
 
-model_out %>% select(seed, risks.overall) %>% 
+model_out %>% 
+  dplyr::select(seed, risks.overall) %>% 
   mutate(seed = as.character(seed)) %>% 
   unnest(cols = c(risks.overall)) %>% 
+  group_by(seed) %>% 
+  filter(any(quantile < 0.3) & !any(est < -0.1)) %>% 
   ggplot(aes(quantile, est, ymin = est - 1.96*sd, ymax = est + 1.96*sd, group = seed)) +
   geom_hline(yintercept = 00, linetype = "dashed", color = "red") +
   geom_pointrange(aes(color = seed), size = .5) + 
   theme_bw() +
-  theme(legend.position = "none") +
+  #theme(legend.position = "none") +
   labs(title = "Overall Mixture Effect over 100 Model Seeds",
        x = "Quantile", y = "Estimate")
 
-model_out %>% select(seed, risks.overall) %>% 
+model_out %>% 
+  dplyr::select(seed, risks.overall) %>% 
   mutate(seed = as.character(seed)) %>% 
   unnest(cols = c(risks.overall)) %>%
   group_by(seed) %>% 
